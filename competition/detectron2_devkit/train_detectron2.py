2a6a93e42ac60c22e040811224284e11fa0b99d4
houbenzhou <houbenzhou@supermap.com>
2020-06-16 15:56:05 +0800

    训练过程硬编码的几个参数，用配置文件来管理

--- competition/detectron2_devkit/train_detectron2.py
+++ competition/detectron2_devkit/train_detectron2.py
@@ -17,12 +17,12 @@ def train_iobjectspy_voc(train_data_path, train_config_path, weight_path, max_it
     cfg.merge_from_file(train_config_path)
     cfg.DATASETS.TRAIN = (register_train_name,)
     cfg.DATASETS.TEST = ()  # no metrics implemented for this dataset
-    cfg.DATALOADER.NUM_WORKERS = 2
+    # cfg.DATALOADER.NUM_WORKERS = 2
     cfg.MODEL.WEIGHTS = weight_path  # initialize from model zoo
-    cfg.SOLVER.IMS_PER_BATCH = 2
-    cfg.SOLVER.BASE_LR = 0.00025
+    # cfg.SOLVER.IMS_PER_BATCH = 2
+    # cfg.SOLVER.BASE_LR = 0.00001
     cfg.SOLVER.MAX_ITER = max_iter  # 300 iterations seems good enough, but you can certainly train longer
-    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128  # faster, and good enough for this toy dataset
+    # cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128  # faster, and good enough for this toy dataset
     num_class = get_class_num(train_data_path)
     cfg.MODEL.ROI_HEADS.NUM_CLASSES = num_class  # get classes from sda
     cfg.OUTPUT_DIR = out_dir
@@ -33,33 +33,6 @@ def train_iobjectspy_voc(train_data_path, train_config_path, weight_path, max_it
     trainer.train()
 
 
-# tree
-# def get_parser():
-#     parser = argparse.ArgumentParser(description="train detectron2")
-#     parser.add_argument(
-#         "--train_data_path",
-#         default="/home/data/hou/workspaces/iobjectspy2/resources_ml/example/项目/tree/out/voc",
-#         help="path to train data directory",
-#     )
-#
-#     parser.add_argument(
-#         "--train_config_path",
-#         default='/home/data/hou/workspaces/detectron2/configs/my_experiment/cascade_mask_rcnn_R_50_FPN_1x.yaml',
-#         help="path to config file",
-#     )
-#
-#     parser.add_argument(
-#         "--weight_path",
-#         default='/home/data/hou/workspaces/detectron2/data/model/model/ablations_for_deformable_conv_and_cascade_rcnn/cascade_mask_rcnn_R_50_FPN_3x/model_final_480dd8.pkl',
-#         help="path to pre training model ",
-#     )
-#     parser.add_argument(
-#         "--out_dir",
-#         default='/home/data/hou/workspaces/my_knowledge_base/competition/detectron2_devkit/out/2020_05_26/tree/model',
-#         help="path to output directory",
-#     )
-#
-#     return parser
 # dota
 def get_parser():
     parser = argparse.ArgumentParser(description="train detectron2")
