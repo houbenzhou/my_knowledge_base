1aceb8e1ba7f64959175cdd08659df4150734de7
houbenzhou <houbenzhou@supermap.com>
2020-12-01 14:32:08 +0800

    通过whl包编译conda包

---
+++ docker/compile_tensorflow/build_conda_package/linux/tensorflow_115_cp36/build.sh
@@ -0,0 +1,3 @@
+if [ `uname` == Linux ]; then
+        pip install /home/data/hou/workspaces/my_knowledge_base/docker/compile_tensorflow/whl_package/linux/tensorflow_115_cp36/tensorflow-1.15.4-cp36-cp36m-linux_x86_64.whl
+fi
