9a116da0e6e66b6b2597099b1c4805645750d8d6
houbenzhou <houbenzhou@supermap.com>
2021-01-07 18:35:53 +0800

    高分4数据可视化验证集标签,计算baseline评估指标

---
+++ competition/gaofen/International/airplane/experiments/r3det_baseline/eval_more.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+/home/data/hou/soft/conda/envs/iobjectspy-gpu/bin/python  \
+/home/data/hou/workspaces/iobjectspy3/resources_ml/example/1_evaluation_metrics.py \
+--y_true /home/hou/data/hou/experiments/gaofen4/result_data/label_visual/Annotations \
+--y_pred /home/hou/data/hou/experiments/gaofen4/result_data/r3det_baseline/result_label_file_more
